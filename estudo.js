8fda2c0d0510c24d313bc306a8e0b54c030535ac
anasara006 <110102304+anasara006@users.noreply.github.com>
2022-09-29 09:07:38 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,17 @@
+let tabuada = 2;
+
+function escreva(){
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    document.write(tabuada + "x 1 ="+ (tabuada*1)+"<br>");
+    
+}
+
 
